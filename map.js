b956b8f6c4d6431b301bca67186d49bd14edda7c
Esther Kinyanjui <estherkinyanjui@yoda-yoda-Air.local>
2024-11-13 15:51:47 -0500

    connected mongo api to map

--- map.js
+++ map.js
@@ -7,6 +7,26 @@ const map = new mapboxgl.Map({
   container: "map", // container ID for the map object (this points to the HTML element)
   style: "mapbox://styles/efaith1/cm187o7vv00ac01nw2eld0qpl",
   center: [-73.9442, 40.6482], // starting position [lng, lat] (google brooklyn)
-  zoom: 11, // starting zoom (adjust it as you wish)
+  zoom: 12, // starting zoom (adjust it as you wish)
   projection: "globe", // display the map as a 3D globe
+});
+
+// Added part
+map.on('load', () => {
+  // Add a GeoJSON source with data from the API
+  map.addSource('mongoLayer', {
+    type: 'geojson',
+    data: 'http://localhost:3000/api/geojson' // Replace with your API endpoint
+  });
+
+  // Add a layer to display MongoDB data
+  map.addLayer({
+    id: 'mongoLayer',
+    type: 'circle', // Options: 'circle', 'line', 'fill'
+    source: 'mongoLayer',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': '#007cbf'
+    }
+  });
 });
